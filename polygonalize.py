8714dc397686061f35303b4772f33189381b8fce
Joel Nises <joel@nises.se>
2015-02-22 18:28:02 +0100

    generate quads more correctly

--- polygonalize.py
+++ polygonalize.py
@@ -20,22 +20,34 @@ tetrahedrons = [x + diagonal for x in (((0, 0, 1), (1, 0, 1)),
                                        ((1, 0, 0), (1, 0, 1)))]
 
 def get_polygons(cube, isovalue):
-    inside = cube > isovalue
+    insidecube = cube > isovalue
+    def calculate_vertex(start, end):
+        if cube[start] < cube[end]:
+            value = (isovalue - cube[start]) / (cube[end] - cube[start])
+        else:
+            value = 1 - (isovalue - cube[end]) / (cube[start] - cube[end])
+        return np.array(start) + (np.array(end) - np.array(start)) * value
     for t in tetrahedrons:
-        polygon = []
-        for start, end in itertools.combinations(t, 2):
-            if inside[start] != inside[end]:
-                if cube[start] < cube[end]:
-                    value = (isovalue - cube[start]) / (cube[end] - cube[start])
-                else:
-                    value = 1 - (isovalue - cube[end]) / (cube[start] - cube[end])
-                polygon.append(np.array(start) + (np.array(end) - np.array(start)) * value)
-        if len(polygon):
-            yield polygon
+        inside = [s for s in t if insidecube[s]]
+        outside = [s for s in t if not insidecube[s]]
+        if len(inside) == 2:
+            yield [calculate_vertex(inside[0], outside[0]),
+                   calculate_vertex(inside[0], outside[1]),
+                   calculate_vertex(inside[1], outside[1]),
+                   calculate_vertex(inside[1], outside[0])]
+        elif len(inside) == 1:
+            yield [calculate_vertex(inside[0], outside[0]),
+                   calculate_vertex(inside[0], outside[1]),
+                   calculate_vertex(inside[0], outside[2])]
+        elif len(inside) == 3:
+            yield [calculate_vertex(outside[0], inside[0]),
+                   calculate_vertex(outside[0], inside[1]),
+                   calculate_vertex(outside[0], inside[2])]
 
 def polygonalize(indata, outfile, isovalue):
     assert len(indata.shape) == 3
     assert all(np.array(indata.shape) > 1)
+    vertexnumber = 0
     for z in range(indata.shape[0] - 1):
         for y in range(indata.shape[1] - 1):
             for x in range(indata.shape[2] - 1):
@@ -45,8 +57,9 @@ def polygonalize(indata, outfile, isovalue):
                     for polygon in (a + np.array((z, y, x)) for a in get_polygons(cube, isovalue)):
                         for vertex in polygon:
                             outfile.write('v {} {} {}\n'.format(vertex[2], vertex[1], vertex[0]))
+                            vertexnumber += 1
                         if len(polygon):
-                            outfile.write('f {}\n'.format(' '.join((str(a) for a in -1 - np.arange(len(polygon))))))
+                            outfile.write('f {}\n'.format(' '.join((str(vertexnumber - a) for a in range(len(polygon))))))
         sys.stdout.write('\r{}/{}'.format(z + 1, indata.shape[0] - 1))
     sys.stdout.write('\n')
 
