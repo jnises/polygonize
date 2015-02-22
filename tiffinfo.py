0e7d57d53d02c11926d22f5c413956dea954b91c
Joel Nises <joel@nises.se>
2015-02-22 21:31:43 +0100

    added util to print the size of a tif

---
+++ tiffinfo.py
@@ -0,0 +1,11 @@
+#/usr/bin/env python3
+
+import tifffile
+import numpy as np
+import argparse
+
+if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('infile')
+    args = p.parse_args()
+    print('{}'.format(tifffile.imread(args.infile).shape))
